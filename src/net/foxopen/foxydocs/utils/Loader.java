54b7135b070d559c7e285b5815d550ef747c41c0
Pierre-Dominique Putallaz <pierredominique.putallaz@gmail.com>
2013-02-28 17:06:03 +1100

    Minor bug fix

--- src/net/foxopen/foxydocs/utils/Loader.java
+++ src/net/foxopen/foxydocs/utils/Loader.java
@@ -5,12 +5,12 @@ All rights reserved.
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-    * Redistributions of source code must retain the above copyright notice, 
+ * Redistributions of source code must retain the above copyright notice, 
       this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright notice, 
+ * Redistributions in binary form must reproduce the above copyright notice, 
       this list of conditions and the following disclaimer in the documentation 
       and/or other materials provided with the distribution.
-    * Neither the name of the DEPARTMENT OF ENERGY AND CLIMATE CHANGE nor the
+ * Neither the name of the DEPARTMENT OF ENERGY AND CLIMATE CHANGE nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
@@ -25,7 +25,7 @@ ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-*/
+ */
 package net.foxopen.foxydocs.utils;
 
 import static net.foxopen.foxydocs.FoxyDocs.*;
@@ -57,7 +57,7 @@ public class Loader {
     // Kill running WatchDog Thread
     if (FoxyDocs.WATCHDOG != null)
       FoxyDocs.WATCHDOG.interrupt();
-    for(CTabItem tab : FoxyDocsMainWindow.tabFolder.getItems()){
+    for (CTabItem tab : FoxyDocsMainWindow.tabFolder.getItems()) {
       tab.dispose();
     }
     return new ThreadPopulateStructure(target);
@@ -107,7 +107,7 @@ public class Loader {
         // Parse modules
         monitor.beginTask("Parsing FoxModules", modules.size());
         monitor.subTask("Parsing " + modules.size() + " FoxModules");
-        
+
         for (FoxModule f : modules.values()) {
           if (monitor.isCanceled())
             break;
@@ -141,8 +141,10 @@ public class Loader {
 
           @Override
           public void modified(Path entryPath) {
-            resolv(entryPath).getParent().firePropertyChange("children", null, resolv(entryPath).getParent().getChildren());
-            resolv(entryPath).refreshUI();
+            if (entryPath != null) {
+              resolv(entryPath).getParent().firePropertyChange("children", null, resolv(entryPath).getParent().getChildren());
+              resolv(entryPath).refreshUI();
+            }
           }
 
           @Override
