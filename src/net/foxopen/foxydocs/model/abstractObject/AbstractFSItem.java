8daa0ba78f05a2c9fccf8a8a340c524e5d9fb3fe
Pierre-Dominique Putallaz <pierredominique.putallaz@gmail.com>
2013-03-29 13:49:33 +1100

    Update header to Fivium's one

--- src/net/foxopen/foxydocs/model/abstractObject/AbstractFSItem.java
+++ src/net/foxopen/foxydocs/model/abstractObject/AbstractFSItem.java
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2013, ENERGY DEVELOPMENT UNIT (INFORMATION TECHNOLOGY)
+Copyright (c) 2013, Fivium Ltd.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
@@ -10,7 +10,7 @@ are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright notice, 
       this list of conditions and the following disclaimer in the documentation 
       and/or other materials provided with the distribution.
-    * Neither the name of the DEPARTMENT OF ENERGY AND CLIMATE CHANGE nor the
+    * Neither the name of Fivium Ltd nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
@@ -24,6 +24,8 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
 ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+$Revision$
 
 */
 package net.foxopen.foxydocs.model.abstractObject;
