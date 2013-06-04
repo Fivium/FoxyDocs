cd3c4c6baf786ab62d7198402df1b1127b74b11c
Pierre-Dominique Putallaz <pierredominique.putallaz@gmail.com>
2013-06-04 12:58:09 +1000

    Update the copyright header to the Fivium's one

--- src/net/foxopen/foxydocs/utils/Loader.java
+++ src/net/foxopen/foxydocs/utils/Loader.java
@@ -1,16 +1,16 @@
 /*
-Copyright (c) 2013, ENERGY DEVELOPMENT UNIT (INFORMATION TECHNOLOGY)
+Copyright (c) 2013, Fivium Ltd.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
- * Redistributions of source code must retain the above copyright notice, 
+    * Redistributions of source code must retain the above copyright notice, 
       this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright notice, 
+    * Redistributions in binary form must reproduce the above copyright notice, 
       this list of conditions and the following disclaimer in the documentation 
       and/or other materials provided with the distribution.
- * Neither the name of the DEPARTMENT OF ENERGY AND CLIMATE CHANGE nor the
+    * Neither the name of Fivium Ltd nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
